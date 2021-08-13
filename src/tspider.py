ed5854d00ca17983317cb6953f463b13b6bfadc9
iptoday <wangdong1221@outlook.com>
2021-08-13 20:23:42 +0800

    init

---
+++ src/tspider.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File: tspider.py
+@Description: 
+@Time: 2021/08/13 20:21:28
+@Author: iptoday
+@Email: wangdong1221@outlook.com
+@Version: 1.0
+'''
+
+import json
+import requests
+
+
+def main():
+    print("项目开始")
+
+
+if __name__ == '__main__':
+    main()
