28e444faeba950ae5a781a67d373992531a94479
iptoday <wangdong1221@outlook.com>
2021-08-13 20:24:31 +0800

    init

---
+++ mian.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File: mian.py
+@Description: 
+@Time: 2021/08/13 20:24:13
+@Author: iptoday
+@Email: wangdong1221@outlook.com
+@Version: 1.0
+'''
+
+# here put the import lib
+import tspider
